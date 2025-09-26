8f80d37c786baf5e0ddce7661a04176db381b70a
Louise Moxhay <louisemoxy@gmail.com>
2025-09-26 16:41:49 +0100

    remove unused posts and favicon

--- pages/_document.js
+++ pages/_document.js
@@ -9,6 +9,7 @@ class MyDocument extends Document {
     return (
       <Html lang="en" className="theme-compiled">
         <Head>
+          <link rel="icon" href="/favicon.png" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
           <link
