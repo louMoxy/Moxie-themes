654631b27283799f9cc582f74ee4d1ed26a1431d
Louise Moxhay <louisemoxy@gmail.com>
2025-10-12 10:46:56 +0100

    Fix netlify form 5

--- pages/_document.js
+++ pages/_document.js
@@ -26,14 +26,6 @@ class MyDocument extends Document {
         <body
           className={`antialiased text-lg leading-base`}
         >
-          {/* Hidden form for Netlify form detection */}
-          <form name="contact" data-netlify="true" netlify-honeypot="bot-field" hidden>
-            <input type="text" name="name" />
-            <input type="email" name="email" />
-            <textarea name="message"></textarea>
-            <input type="hidden" name="bot-field" />
-          </form>
-          
           <Main />
           <NextScript />
         </body>
